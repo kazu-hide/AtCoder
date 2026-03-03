b4959efa8af025258e3df88a51769317ffc25fa4
Kazu <64197690+ZawaPaP@users.noreply.github.com>
2026-03-04 07:25:33 +0900

    create lcm and 素数判定

--- lcm.py
+++ lcm.py
@@ -1,1 +1,15 @@
 # 最小公倍数
+
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a // gcd(a, b) * b
+
+
+x, y = map(int, input().split())
+print(lcm(x, y))
--- 素数判定.py
+++ 素数判定.py
@@ -1,1 +1,20 @@
 # 素数判定
+
+
+def get_prime_numbers(x):
+    if x < 2:
+        return []
+
+    is_prime = [True] * (x + 1)
+    is_prime[0] = is_prime[1] = False
+
+    for p in range(2, int(x**0.5) + 1):
+        if is_prime[p]:
+            for i in range(p * p, x + 1, p):
+                is_prime[i] = False
+
+    return [p for p in range(2, x + 1) if is_prime[p]]
+
+
+x = int(input())
+print(get_prime_numbers(x))
